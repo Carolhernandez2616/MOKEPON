4b6502ceff89e8663502f4fc03f080fdc4426321
Carolhernandez2616 <hernandezcuervocarolyurley@gmail.com>
2026-01-29 21:42:33 +0000

    Clase 47 - Curso de programación básica en PLATZI

--- js/Mokepon.js
+++ js/Mokepon.js
@@ -28,6 +28,18 @@ let ataqueEnemigo
 let vidasJugador = 3
 let vidasEnemigo = 3
 
+class MOKEPON {
+  constructor(nombre, foto, vida){
+    this.nombre = nombre
+    this.foto = foto
+    this.vida = vida
+  }
+}
+
+let Hipodoge = new MOKEPON("Hipodoge", "./assets/HIPODOGE.png", 5)
+let Capipepo = new MOKEPON("Capipepo", "./assets/CAPIPEPO.png", 5)
+let Ratigueya = new MOKEPON("Ratigueya", "./assets/RATIGUEYA.png", 5)
+
 
 function iniciarJuego (){
   
